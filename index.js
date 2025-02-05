da3a4f6b73cd72afa0e7590dda619ec14ec92126
Sunggat Alimbetov <sunggatalimbetov@gmail.com>
2025-02-05 17:50:10 +0500

    gpio service commented

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import { config } from "./config/env.js";
 import { corsOptions } from "./middleware/cors.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 import { SocketHandler } from "./socket/handler.js";
-import { GPIOService } from "./services/gpio.js";
+// import { GPIOService } from "./services/gpio.js";
 import healthRoutes from "./routes/health.js";
 import faceRoutes from "./routes/face.js";
 import cameraRoutes from "./routes/camera.js";
@@ -17,7 +17,7 @@ dotenv.config();
 
 const app = express();
 
-GPIOService.init();
+// GPIOService.init();
 
 app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
--- services/gpio.js
+++ services/gpio.js
@@ -1,52 +1,55 @@
-import { Gpio } from "onoff";
-import { GPIO_PINS } from "../services/sensors/constants.js";
-import { getTemperatureValue } from "./sensors/temperature.js";
-import { getAlcoholValue } from "./sensors/alcohol.js";
-import { getPulseValue } from "./sensors/pulse.js";
+// import { Gpio } from "onoff";
+// import { getTemperatureValue } from "./sensors/temperature.js";
+// import { getAlcoholValue } from "./sensors/alcohol.js";
+// import { getPulseValue } from "./sensors/pulse.js";
 
-export class GPIOService {
-	static gpios = {};
+// const PINS = {
+// 	ALCOHOL_POWER: 23,
+// 	ALCOHOL_SOBER: 27,
+// 	ALCOHOL_DRUNK: 22,
+// 	ALCOHOL_READY: 17,
+// 	ALCOHOL_TOGGLE: 14,
+// };
 
-	static init() {
-		try {
-			// Initialize output pins
-			[
-				GPIO_PINS.HEARTBEAT,
-				GPIO_PINS.TEMPERATURE,
-				GPIO_PINS.ALCOHOL,
-			].forEach((pin) => {
-				this.gpios[pin] = new Gpio(pin, "out");
-			});
-		} catch (error) {
-			console.error("GPIO initialization failed:", error);
-			process.exit(1);
-		}
+// export class GPIOService {
+// 	static gpios = {};
 
-		// Cleanup GPIO on process exit
-		process.on("SIGINT", () => {
-			Object.values(this.gpios).forEach((gpio) => gpio.unexport());
-		});
-	}
+// 	static init() {
+// 		try {
+// 			// Initialize output pins
+// 			[PINS.HEARTBEAT, PINS.TEMPERATURE, PINS.ALCOHOL].forEach((pin) => {
+// 				this.gpios[pin] = new Gpio(pin, "out");
+// 			});
+// 		} catch (error) {
+// 			console.error("GPIO initialization failed:", error);
+// 			process.exit(1);
+// 		}
 
-	static writePin(pin, value) {
-		this.gpios[pin].writeSync(value > 128 ? 1 : 0);
-	}
+// 		// Cleanup GPIO on process exit
+// 		process.on("SIGINT", () => {
+// 			Object.values(this.gpios).forEach((gpio) => gpio.unexport());
+// 		});
+// 	}
 
-	static async getHeartbeat() {
-		const pulseData = await getPulseValue();
-		console.log("Pulse Data:", pulseData);
-		return pulseData;
-	}
+// 	static writePin(pin, value) {
+// 		this.gpios[pin].writeSync(value > 128 ? 1 : 0);
+// 	}
 
-	static async getTemperature() {
-		const temperatureData = await getTemperatureValue();
-		console.log("Temperature Data:", temperatureData);
-		return temperatureData;
-	}
+// 	static async getHeartbeat() {
+// 		const pulseData = await getPulseValue();
+// 		console.log("Pulse Data:", pulseData);
+// 		return pulseData;
+// 	}
 
-	static async getAlcohol() {
-		const alcoholData = await getAlcoholValue();
-		console.log("Alcohol Level Data:", alcoholData);
-		return alcoholData;
-	}
-}
+// 	static async getTemperature() {
+// 		const temperatureData = await getTemperatureValue();
+// 		console.log("Temperature Data:", temperatureData);
+// 		return temperatureData;
+// 	}
+
+// 	static async getAlcohol() {
+// 		const alcoholData = await getAlcoholValue();
+// 		console.log("Alcohol Level Data:", alcoholData);
+// 		return alcoholData;
+// 	}
+// }
